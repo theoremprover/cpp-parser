48e5d552eae5cde22e8720563b448f1b3e1790d3
Theoremprover <no@email.com>
2017-11-01 01:05:14 +0100

    No Message

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,79 @@
+{-
+stack build
+stack exec cpp-parser-exe -- test\src\Notepad_plus.cpp
+-}
+
+
 module Main where
 
 import System.Environment
 
 import ParseCPP
 
+includeDirs = [
+	"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Tools\\MSVC\\14.11.25503\\include",
+	"test\\src\\scintilla-include",
+	"test\\src\\WinControls\\AboutDlg",
+	"test\\..\\scintilla\\include",
+	"test\\include",
+	"test\\src\\WinControls",
+	"test\\src\\WinControls\\ImageListSet",
+	"test\\src\\WinControls\\OpenSaveFileDialog",
+	"test\\src\\WinControls\\SplitterContainer",
+	"test\\src\\WinControls\\StaticDialog",
+	"test\\src\\WinControls\\TabBar",
+	"test\\src\\WinControls\\ToolBar",
+	"test\\src\\MISC\\Process",
+	"test\\src\\ScitillaComponent",
+	"test\\src\\MISC",
+	"test\\src\\MISC\\SysMsg",
+	"test\\src\\WinControls\\StatusBar",
+	"test\\src",
+	"test\\src\\WinControls\\StaticDialog\\RunDlg",
+	"test\\src\\tinyxml",
+	"test\\src\\WinControls\\ColourPicker",
+	"test\\src\\Win32Explr",
+	"test\\src\\MISC\\RegExt",
+	"test\\src\\WinControls\\TrayIcon",
+	"test\\src\\WinControls\\shortcut",
+	"test\\src\\WinControls\\Grid",
+	"test\\src\\WinControls\\ContextMenu",
+	"test\\src\\MISC\\PluginsManager",
+	"test\\src\\WinControls\\Preference",
+	"test\\src\\WinControls\\WindowsDlg",
+	"test\\src\\WinControls\\TaskList",
+	"test\\src\\WinControls\\DockingWnd",
+	"test\\src\\WinControls\\ToolTip",
+	"test\\src\\MISC\\Exception",
+	"test\\src\\MISC\\Common",
+	"test\\src\\tinyxml\\tinyXmlA",
+	"test\\src\\WinControls\\AnsiCharPanel",
+	"test\\src\\WinControls\\ClipboardHistory",
+	"test\\src\\WinControls\\FindCharsInRange",
+	"test\\src\\WinControls\\VerticalFileSwitcher",
+	"test\\src\\WinControls\\ProjectPanel",
+	"test\\src\\WinControls\\DocumentMap",
+	"test\\src\\WinControls\\FunctionList",
+	"test\\src\\uchardet",
+	"test\\src\\WinControls\\FileBrowser",
+	"test\\src\\WinControls\\ReadDirectoryChanges",
+	"test\\src\\MISC\\md5",
+	"test\\src\\WinControls\\PluginsAdmin" ]
+
+defines = [
+	"WIN32","_WIN32_WINNT=0x0501","_WINDOWS","_USE_64BIT_TIME_T","TIXML_USE_STL",
+	"TIXMLA_USE_STL" ,"_CRT_NONSTDC_NO_DEPRECATE","_CRT_SECURE_NO_WARNINGS",
+	"_CRT_NON_CONFORMING_SWPRINTFS=1" ,"_CRT_NON_CONFORMING_WCSTOK","_VC80_UPGRADE=0x0700",
+	"_USING_V110_SDK71_","_UNICODE","UNICODE" ]
+
+otherOptions = [
+	"-fexceptions",
+--	"-std=c++14",
+	"-fms-compatibility-version=19",
+	"-w","-Xanalyzer","-analyzer-disable-all-checks" ]
+
+options = otherOptions ++ map ("-I"++) includeDirs ++ map ("-D"++) defines
+
 main = do
 	[file] <- getArgs
-	parseCPP file
+	parseCPP file options
--- src/ParseCPP.hs
+++ src/ParseCPP.hs
@@ -10,17 +10,48 @@ import Control.Lens
 import qualified Data.ByteString.Char8 as BS
 import Data.Monoid
 
-clangOptions :: [String]
-clangOptions = [
-	"-IC:/LLVM/lib/clang/3.8.1/include" ]
-
-parseCPP filepath = do
+parseCPP :: FilePath -> [String] -> IO ()
+parseCPP filepath options = do
 	idx <- createIndex
-	tu <- parseTranslationUnit idx filepath clangOptions
+	tu <- parseTranslationUnit idx filepath options
 
+	let elements = cursorDescendantsF
+		. folding (matchKind @'CXXMethod)
+		. filtered (isFromMainFile . rangeStart . cursorExtent)
+		. to (\funDec -> cursorSpelling funDec <> " :: " <> typeSpelling (cursorType funDec))
+{-
 	let funDecs = cursorDescendantsF
-		. folding (matchKind @'FunctionDecl)
+		. folding (matchKind @'CXXMethod)
 		. filtered (isFromMainFile . rangeStart . cursorExtent) -- ...that are actually in the given file
 		. to (\funDec -> cursorSpelling funDec <> " :: " <> typeSpelling (cursorType funDec))
+-}
 
-	BS.putStrLn $ BS.unlines (translationUnitCursor tu ^.. funDecs)
+	BS.putStrLn $ BS.unlines (translationUnitCursor tu ^.. elements)
+
+{-
+
+type Fold s a = forall f. (Contravariant f, Applicative f) => (a -> f a) -> s -> f s
+
+folding :: Foldable f => (s -> f a) -> Fold s a
+
+cursorDescendantsF :: Fold (CursorK kind) Cursor
+
+translationUnitCursor :: TranslationUnit -> CursorK TranslationUnit
+
+matchKind :: forall kind. SingI kind => Cursor -> Maybe (CursorK kind)
+
+filtered :: (Choice p, Applicative f) => (a -> Bool) -> Optic' p f a a
+
+to :: (Profunctor p, Contravariant f) => (s -> a) -> Optic' p f s a
+
+(^..) :: s -> Getting (Endo [a]) s a -> [a]
+
+toListOf :: Getting (Endo [a]) s a -> s -> [a]
+
+type Getting r s a = (a -> Const r a) -> s -> Const r s
+
+type Optic' p f s a = Optic p f s s a a
+
+type Optic p f s t a b = p a (f b) -> p s (f t)
+
+-}
